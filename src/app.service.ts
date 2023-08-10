26545bc593e4f002b49ed2a4df3ba9443825605f
Seohyun Yoon <sarahseohyun@gmail.com>
2023-08-10 11:53:51 +0000

    change hello return string

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello seohyun!';
+    return 'Hello seohyun! New!';
   }
 }
